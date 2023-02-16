364334b759bb01752d5586f183425f13bf23eef9
kizzykrishh <kingsleyofulume4@gmail.com>
2023-02-16 14:44:47 -0800

    hello print f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: read 0 if correct
- */
+* main - Entry
+* Return: read 0 if correct
+*/
+
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	puts("\"Programming is like building a multilingual puzzle");           	return (0); 
 }	
