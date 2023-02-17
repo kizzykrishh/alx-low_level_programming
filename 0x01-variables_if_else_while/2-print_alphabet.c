601f5a2a5156a5374a58eb59510c5379d934bcb5
kizzykrishh <kingsleyofulume4@gmail.com>
2023-02-17 15:25:41 -0800

    I sometimes suffer from insomia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Return: print 0 is correct
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
