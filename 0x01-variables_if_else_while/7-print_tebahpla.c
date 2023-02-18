c73eab2a7375123ef99aef9d7fa2fae4bae08c36
kizzykrishh <kingsleyofulume4@gmail.com>
2023-02-18 02:02:49 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: print 0 if correct
+ */
+int main(void)
+{
+	char u;
+
+		for (u = 'z' ; u >= 'a' ; u--)
+			putchar(u);
+	putchar('\n');
+	return (0);
+}
+
