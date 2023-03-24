dec120a3b820e881827ed797dd859b4a6824c7a9
kizzykrishh <kingsleyofulume4@gmail.com>
2023-03-24 16:46:45 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_POINTERS_H
+#define FUNCGTIONS_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
